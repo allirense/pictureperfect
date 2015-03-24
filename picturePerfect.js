160db6c9b98043a497730c8e453a18924a75b529
Alli <allirense@gmail.com>
2015-03-23 21:12:36 -0400

    Fixed plugin initialization

--- picturePerfect.js
+++ picturePerfect.js
@@ -6,49 +6,52 @@
 
 (function($){
   $.fn.picturePerfect = function(selector) {
-    imagesLoaded($('.picture-perfect'), function() {
-      $('.picture-perfect').find(selector).children('img').each(function() {
-          var image = new Image();
-          image.src = $(this).attr('src');
-          var h = image.naturalHeight;
-          var w = image.naturalWidth;
-          var newWidth;
-          var newHeight;
-          var centered = true;
-          var parentWidth = $(this).parent('div').width();
-          var parentHeight = $(this).parent('div').height();
-          // Set orientation and height or width
-          if (h > w) {
-            var orientation = 'portrait';
-            $(this).css({
-              'width' : '100%',
-              'height' : 'auto'
-            });
-            newWidth = $(this).width();
-            newHeight = $(this).height();
-          } else if (w > h) {
-            var orientation = 'landscape';
-            $(this).css({
-              'width' : 'auto',
-              'height' : '100%'
-            });
-            newWidth = $(this).width();
-            newHeight = $(this).height();
-          } else {
-            var orientation = 'square';
-            $(this).css({
-              'width' : '100%',
-              'height' : '100%'
-            });
-          }
-          var heightOffset = (parentHeight - newHeight)/2;
-          var widthOffset = (parentWidth - newWidth)/2
-          // Center image horizontally or vertically as needed
-          if (centered && orientation === 'portrait') {
-              $(this).css('top',heightOffset + 'px');
-          } else if (centered && orientation === 'landscape') {
-            $(this).css('left',widthOffset + 'px');
-          } 
+    this.each( function() {
+      var $this = $(this);
+      $this.imagesLoaded().always(function(instance) {
+        $this.find(selector).children('img').each(function() {
+            var image = new Image();
+            image.src = $(this).attr('src');
+            var h = image.naturalHeight;
+            var w = image.naturalWidth;
+            var newWidth;
+            var newHeight;
+            var centered = true;
+            var parentWidth = $(this).parent('div').width();
+            var parentHeight = $(this).parent('div').height();
+            // Set orientation and height or width
+            if (h > w) {
+              var orientation = 'portrait';
+              $(this).css({
+                'width' : '100%',
+                'height' : 'auto'
+              });
+              newWidth = $(this).width();
+              newHeight = $(this).height();
+            } else if (w > h) {
+              var orientation = 'landscape';
+              $(this).css({
+                'width' : 'auto',
+                'height' : '100%'
+              });
+              newWidth = $(this).width();
+              newHeight = $(this).height();
+            } else {
+              var orientation = 'square';
+              $(this).css({
+                'width' : '100%',
+                'height' : '100%'
+              });
+            }
+            var heightOffset = (parentHeight - newHeight)/2;
+            var widthOffset = (parentWidth - newWidth)/2
+            // Center image horizontally or vertically as needed
+            if (centered && orientation === 'portrait') {
+                $(this).css('top',heightOffset + 'px');
+            } else if (centered && orientation === 'landscape') {
+              $(this).css('left',widthOffset + 'px');
+            } 
+        });
       });
     });
   }
