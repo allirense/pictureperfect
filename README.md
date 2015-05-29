6e393db459e1b060542d9611b64162b44d4e0aa3
allirense <allirense@gmail.com>
2015-05-29 15:12:35 -0400

    Added changelog. Added options. Added support for non-square containers.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # pictureperfect
-Get your images to fit in their container. Squares? Circles? No problem.
+Get your images to fit in their container. Squares? Circles? Rectangles? No problem.
+
+## Version 1.0.0
+
+Version 1.0.0 has been released!
+
+This version is not backwards compatible with previous versions. Please see the CHANGELOG for more details.
+
+The previous version(s) is depreciated.
--- picturePerfect.js
+++ picturePerfect.js
@@ -1,56 +1,102 @@
 /*!
- * PicturePerfect.js v 0.1
+ * PicturePerfect.js v 1.0.0
  * Contain your images!
+ * Bult by Alli Rense www.allirense.com
  * MIT License
  */
 
-(function($){
-  $.fn.picturePerfect = function(selector) {
-    this.each( function() {
+(function ($) {
+  'use strict';
+  $.fn.picturePerfect = function(options) {
+    // Options!
+    var defaults = {
+      selector: '.image-container',
+      centered: true
+    },
+    settings = $.extend({}, defaults, options);
+    this.each( function() {;
       var $this = $(this);
       $this.imagesLoaded().always(function(instance) {
-        $this.find(selector).children('img').each(function() {
+        $this.find(settings.selector).children('img').each(function() {
             var image = new Image();
-            image.src = $(this).attr('src');
+            var thisImg = $(this);
+            image.src = thisImg.attr('src');
             var h = image.naturalHeight;
             var w = image.naturalWidth;
+            var imageRatio = h/w;
+            var parentWidth = thisImg.parent('div').width();
+            var parentHeight = thisImg.parent('div').height();
+            var parentRatio = parentHeight/parentWidth;
+            var heightDif = parentHeight - h;
+            var ratioDif = parentRatio - imageRatio;
+            // Define Resizing Functions
             var newWidth;
             var newHeight;
-            var centered = true;
-            var parentWidth = $(this).parent('div').width();
-            var parentHeight = $(this).parent('div').height();
-            // Set orientation and height or width
-            if (h > w) {
-              var orientation = 'portrait';
-              $(this).css({
+            function heightAuto() {
+              thisImg.css({
                 'width' : '100%',
                 'height' : 'auto'
               });
-              newWidth = $(this).width();
-              newHeight = $(this).height();
-            } else if (w > h) {
-              var orientation = 'landscape';
-              $(this).css({
+              newWidth = thisImg.width();
+              newHeight = thisImg.height();
+            }
+            function widthAuto() {
+              thisImg.css({
                 'width' : 'auto',
                 'height' : '100%'
               });
-              newWidth = $(this).width();
-              newHeight = $(this).height();
-            } else {
-              var orientation = 'square';
-              $(this).css({
+              newWidth = thisImg.width();
+              newHeight = thisImg.height();
+            }
+            function bothFull() {
+              thisImg.css({
                 'width' : '100%',
                 'height' : '100%'
               });
             }
+            // Determine Height Difference
+            var hDif;
+            if (heightDif === 0) {
+              hDif = 'none'
+            } else if (heightDif > 0) {
+              hDif = 'pos'
+            } else {
+              hDif = 'neg'
+            }
+            // Determine Ratio Difference
+            var rDif;
+            if (ratioDif === 0) {
+              rDif = 'none'
+            } else if (ratioDif > 0) {
+              rDif = 'pos'
+            } else {
+              rDif = 'neg'
+            }
+            // Run resizing function depending on Height and Ratio Difference
+            var auto;
+            if (rDif === 'none') {
+              var auto = 'none';
+              bothFull();
+            } else if ((hDif === 'pos' && rDif === 'pos') || (hDif === 'neg' && rDif === 'pos') || (hDif === 'none' && rDif === 'neg')) {
+              var auto = 'width'
+              widthAuto();
+            } else if ((hDif === 'none' && rDif === 'pos') || (hDif === 'pos' && rDif === 'neg') || (hDif === 'neg' && rDif === 'neg')) {
+              var auto = 'height';
+              heightAuto();
+            }
+            // Center image horizontally or vertically as needed
             var heightOffset = (parentHeight - newHeight)/2;
             var widthOffset = (parentWidth - newWidth)/2
-            // Center image horizontally or vertically as needed
-            if (centered && orientation === 'portrait') {
-                $(this).css('top',heightOffset + 'px');
-            } else if (centered && orientation === 'landscape') {
-              $(this).css('left',widthOffset + 'px');
-            } 
+            if (settings.centered) {
+              if (auto === 'height') {
+                thisImg.css('top',heightOffset + 'px');
+              } else if (auto === 'width') {
+                thisImg.css('left',widthOffset + 'px');
+              } else {
+                thisImg.css('top',heightOffset + 'px');
+                thisImg.css('left',widthOffset + 'px');
+              }
+            }
         });
       });
     });
